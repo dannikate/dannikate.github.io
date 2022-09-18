cb59f40c0a3a7464903c7c3827e5b58b3faf1444
Dannika <71364766+dannikate@users.noreply.github.com>
2022-09-18 22:54:39 +1000

    added compt for certifications

---
+++ src/components/Certifications.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Certifications() { 
+    return (
+        <div>
+            <h2>/ certifications</h2>
+            <ul>
+                <li>Social-Behavioral-Education Researchers, CITI Program. Issued December 2021, Expires November 2024.</li>
+            </ul>
+        </div>
+    )
+}
