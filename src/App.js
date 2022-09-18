0a2ceb8f923b98e9a74d93e76173f18ecd1b4049
Dannika <71364766+dannikate@users.noreply.github.com>
2022-09-18 17:05:07 +1000

    updated App and Main

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Main from "./Main"
+
+export default function App() {
+    return (
+        <div>
+            <h1>Hi! This is loading from App.</h1>
+        </div>
+    )
+}
---
+++ src/Main.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <div>
+            <h1>This is from the Main component.</h1>
+        </div>
+    )
+}
