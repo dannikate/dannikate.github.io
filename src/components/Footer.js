2ee491e4ac03f961ea558a756ac12c73805c7292
Dannika <71364766+dannikate@users.noreply.github.com>
2022-09-18 23:03:21 +1000

    added a compt for papers'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <p>Created by Dannika Kate Valerio. All rights reserved,2022. </p>
+        <p>Created by Dannika Kate Valerio. All rights reserved, 2022. </p>
     )
 }
---
+++ src/components/Papers.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function Papers() {
+    return (
+        <div>
+            <h2>/ papers</h2>
+            <ul>
+                <li>Exploring the Relationship Between Susceptibility to Misinformation and Attitudes Towards Local Politics.</li>
+                    <ul>
+                        <li>August 2021 - December 2021</li>
+                        <li>Collaborator: Maxon William</li>
+                        <li>Associated with the University of Guam's College of Liberal Arts and Social Sciences.</li>
+                        <li>Presented online at the 43rd Annual Research Conference (ARC) for the College of Liberal Arts and Social Sciences at the University of Guam on March 11, 2022.</li>
+                        <li>Abstract: Fake news, misinformation, and news media literacy are matters of concern with the prevalence of news information across online social media platforms. This study examines the relationship between self-perceived susceptibility to misinformation and attitudes towards local politics, particularly political cynicism and political skepticism. An online survey was conducted through Qualtrics and ran from October to December 2020, sampling the Guam population. The results showed no relationship between an individual’s perceived susceptibility to misinformation and political cynicism; however, it did show that the relationship between an individual’s perceived susceptibility to misinformation was significant. This suggests that individuals with lower levels of perceived susceptibility to misinformation are more skeptical towards local politics. The results of this study also suggest the implications of susceptibility to misinformation as a component of media literacy curriculums in local schools and, in turn, how these attitudes can cultivate engagement in local politics.</li>
+                        <li>This research was completed under the advisement of Dr. Francis Dalisay, PhD as part of the senior capstone course for the Communication program at the University of Guam.</li>
+                    </ul>
+            </ul>
+        </div>
+    )
+}
