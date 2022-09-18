40749fe51fa3525c7e864cbac2ffb6d9debc9c81
Dannika <71364766+dannikate@users.noreply.github.com>
2022-09-19 00:12:37 +1000

    updated styles and footer compt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <p>Created by Dannika Kate Valerio. All rights reserved, 2022. </p>
+        <div className="footer">
+            <p>Created by Dannika Kate Valerio. All rights reserved, 2022.</p>
+        </div>
     )
 }
