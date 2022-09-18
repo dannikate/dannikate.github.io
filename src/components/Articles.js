31cc6c8544d9ad0ea4b79966f7024d9175342314
Dannika <71364766+dannikate@users.noreply.github.com>
2022-09-19 00:08:11 +1000

    added TODO in articles compt

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -2,6 +2,7 @@ import React from "react"
 
 export default function Articles() {
     return (
+        //TODO: Update and add props; articles will be JS objects in a JS file named 'articles-list'
         <div className="articles">
             <h3> > articles </h3>
             <p>(insert carousel of articles here)</p>
--- src/components/Papers.js
+++ src/components/Papers.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Papers() {
     return (
         <div className="papers">
-            <h2>/ papers</h2>
+            <h3>> papers</h3>
             <ul>
                 <li>Exploring the Relationship Between Susceptibility to Misinformation and Attitudes Towards Local Politics</li>
                     <ul>
