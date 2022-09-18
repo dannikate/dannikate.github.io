c12ac428d9b9f5f18acb53b454ebb6f3c25cbf20
Dannika <71364766+dannikate@users.noreply.github.com>
2022-09-18 22:51:29 +1000

    updates to footer and education compts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from "react"
-// import Main from "./Main"
 import About from "./components/About"
 import Skills from "./components/Skills"
 import Experience from "./components/Experience"
+import Education from "./components/Education"
 import Recognition from "./components/Recognition"
 import Portfolio from "./components/Portfolio"
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 
 export default function App() {
     return (
         <div>
-            <h1>Hi! This is loading from App.</h1>
+            <h1>Hi, I'm Dannika</h1>
             <About />
             <Skills />
             <Experience /> 
+            <Education />
             <Recognition />
             <Portfolio />
             <Contact />
+            <Footer />
         </div>
     )
 }
---
+++ src/components/Education.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Education() {
+    return (
+        <div>
+            <h2>/ education</h2>
+            <ul>
+                <li>University of Guam: August 2017 - December 2021, Magna Cum Laude. Bachelor of Arts in Communication: Mass Media Studies & Journalism. Communication Society Member. </li>
+                <li>University of Guam: August 2017 - December 2021, Magna Cum Laude. Bachelor of Science in Computer Science. Engineering, Mathematics, and Computer Science Club member.</li>
+            </ul>
+        </div>
+    )
+}
