b34685a47f81c14e49b8b0d0e80ccae7372cf6e9
Dannika <71364766+dannikate@users.noreply.github.com>
2022-09-19 22:52:13 +1000

    added data for productions section

---
+++ src/data/productions-list.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        id: 1,
+        title: "Daily Dilemma",
+        year: 2019,
+        positions: "Writer, Director, Editor",
+        synopsis: "Sometimes a daily routine can be the very essence of frustration. A mood piece captured through one of the mundanities of everyday life.",
+        recognition: "Runner-Up for the 'Mood Piece' category at the University of Guam's 6th Annual Film Festival on Nov. 2020.",
+        link: ""
+    },
+    {
+        id: 2,
+        title: "Building Blocks",
+        year: 2020,
+        positions: "Writer, Director, Editor",
+        synopsis: "A solo documentary project for the documentary process course under the instruction of Dr. Raymond Anderson, PhD. This project was completed in the Spring 2020 semester at the University of Guam.",
+        recognition: "",
+        link: ""
+    },
+    {
+        id: 3, 
+        title: "Zero Down",
+        year: 2020,
+        positions: "Editor, Motion Graphics Artist",
+        synopsis: "As economic infrastructures experience sudden crises, a question is brought up: is it becoming futile to prioritize investment in these areas of life such as tourism? Zero Down follows a family that lived in a low-income who shares their thoughts on the subject of living in these areas, a sociology professor who gives insight on socioeconomic disparities, and an individual who is connected to Guam’s tourism industry who delves deep into the topic of fragile markets, all in order to uncover the answer to this timely and thought-provoking question. This project was completed under the instruction and executive production of Dr. Raymond Anderson, PhD.",
+        recognition: "Featured at the 7th Annual University of Guam Film Festival (Spring 2022 Showcase). Awarded as Best Documentary.",
+        link: "https://www.youtube.com/watch?v=DjlhMtGi5t8"
+    },
+    {
+        id: 4, 
+        title: "A Trip East",
+        year: 2020,
+        positions: "Writer, Director, Editor",
+        synopsis: "A twisted trailer putting a horrific spin on Jon M. Chu's 2018 film, Crazy, Rich Asians.",
+        recognition: "Runner-Up for the 'Twisted Trailer' category at the University of Guam's 6th Annual Film Festival on Nov. 2020.",
+        link: ""
+    },
+    {
+        id: 5, 
+        title: "Triton TV News (Season 2020, Episode 2)",
+        year: 2020,
+        positions: "Producer, Writer",
+        synopsis: "A student-based digital news production completed in the Spring 2020 semester for the Broadcast Journalism course at the University of Guam. This second socially-distanced episode of TTN features four stories: uncovers how COVID-19 has negatively affected local restaurants, explores how the destruction of habitats can be a major cause of viral outbreaks, investigates how churches cope with the inability to congregate under new restrictions, and examines how the reliance on local agriculture can be beneficial. This was under the instruction and executive production of Dr. Raymond Anderson, PhD.",
+        recognition: "",
+        link: "https://www.youtube.com/watch?v=NURyvmz9AL8"
+    }
+]
