25cb8db6857d30338c10948d092b1804cf02949a
Dannika <71364766+dannikate@users.noreply.github.com>
2022-09-19 22:59:24 +1000

    added data for list of licenses and certs

---
+++ src/data/certs-licenses.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        id: 1,
+        name: "Social-Behavioral-Educational Researchers",
+        issuer: "CITI Program",
+        issued: "Dec 2021",
+        expiration: "Nov 2024"
+    }
+]
