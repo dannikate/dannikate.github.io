9aca51876827aa6f0d23b35a760723f62823fbc2
Dannika <71364766+dannikate@users.noreply.github.com>
2022-09-19 22:31:05 +1000

    added data for articles section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,26 +4,30 @@ export default function Skills() {
     return (
         <div className="skills">
             <h2>/ skills & competencies</h2>
+            <div className="skills--software">
             <h3>software</h3>
-            <ul>
-                <li>Premiere Pro</li>
-                <li>After Effects</li>
-                <li>Illustrator</li>
-                <li>PhotoShop</li>
-                <li>Audition</li>
-                <li>ProTools</li>
-                <li>Figma</li>
-            </ul>
-            <h3>technologies</h3>
-            <ul>
-                <li>Java</li>
-                <li>Python</li>
-                <li>MatLab</li>
-                <li>Swift</li>
-                <li>HTML & CSS</li>
-                <li>JavaScript</li>
-                <li>Git</li>
-            </ul>
+                <ul>
+                    <li>Premiere Pro</li>
+                    <li>After Effects</li>
+                    <li>Illustrator</li>
+                    <li>PhotoShop</li>
+                    <li>Audition</li>
+                    <li>ProTools</li>
+                    <li>Figma</li>
+                </ul>
+            </div>
+            <div className="skills--technologies">
+                <h3>technologies</h3>
+                <ul>
+                    <li>Java</li>
+                    <li>Python</li>
+                    <li>MatLab</li>
+                    <li>Swift</li>
+                    <li>HTML & CSS</li>
+                    <li>JavaScript</li>
+                    <li>Git</li>
+                </ul>
+            </div>
         </div>
     )
 }
---
+++ src/data/articles-list.js
@@ -0,0 +1,72 @@
+export default [
+    {
+        id: 1,
+        title: "Kho fosters change for protecting the environment", 
+        pubishDate: "Nov 2019",
+        publication: "Triton's Call",
+        link: "https://tritonscall.com/kho-fosters-change-for-protecting-the-environment/"
+    },
+    {
+        id: 2,
+        title: "UOG REU participants share research findings at student colloqium",
+        publishDate: "Nov 2019",
+        publication: "Triton's Call",
+        link: "https://tritonscall.com/uog-reu-participants-share-research-findings-at-student-colloquium/"
+    },
+    {
+        id: 3,
+        title: "MO'NA: A new student orgnization ready to make waves",
+        publishDate: "Dec 2019",
+        publication: "Triton's Call",
+        link: "https://tritonscall.com/mona-a-new-uog-organization-ready-to-make-waves/"
+    },
+    {
+        id: 4,
+        title: "Guam continues to face food insecurity in times of crisis",
+        publishDate: "May 2020",
+        publication: "ArcGIS StoryMaps",
+        link: "https://storymaps.arcgis.com/stories/b87420afb23b439584cce77cfadc10d7"
+    },
+    {
+        id: 5,
+        title: "Harmony Chea: A Local Musician on the Rise",
+        publishDate: "Oct 2020",
+        publication: "Triton's Call",
+        link: "https://tritonscall.com/harmony-chea-a-local-musician-on-the-rise/"
+    }, 
+    {
+        id: 6,
+        title: "Food distribution at Asan-Maina Mayor's office",
+        publishDate: "Nov 2020",
+        publication: "Triton's Call",
+        link: "https://tritonscall.com/food-distribution-at-asan-maina-mayors-office/"
+    },
+    {
+        id: 7,
+        title: "Live Music in COVID-19: Smiles Davis",
+        publishDate: "Dec 2020",
+        publication: "Triton's Call",
+        link: "https://tritonscall.com/live-music-in-covid-19-smiles-davis/"
+    },
+    {
+        id: 8,
+        title: "COVID-19's Impact on Higher Education on Guam",
+        publishDate: "Dec 2020",
+        publication: "ArcGIS StoryMaps",
+        link: "https://storymaps.arcgis.com/stories/8c34e2161db2490aacceb023e90b761c"
+    }, 
+    {
+        id: 9,
+        title: "WERI representatibes present at the Soil and Water Education Symposium",
+        publishDate: "Aug 2021",
+        publication: "WERI News",
+        link: "https://weri.uog.edu/2021/08/09/weri-representatives-present-at-the-soil-and-water-educators-symposium/"
+    }, 
+    {
+        id: 10,
+        title: "University of Pittsburg students visit WERI",
+        publishDate: "Aug 2021",
+        publication: "WERI News",
+        link: "https://weri.uog.edu/2021/08/17/upitt-visits-weri/"
+    }
+]
