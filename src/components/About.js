2ab474b7ffeb2af29b2389be26b43ec7879c041e
Dannika <71364766+dannikate@users.noreply.github.com>
2023-04-05 14:39:45 +1000

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div className="about">
             <h2>/ about</h2>
-                <p>A graduate from the University of Guam, where I studied Computer Science and Communication. Currently working as a Research Assistant II at the Water and Environmental Research Institute of the Western Pacific (WERI). An aspiring [web] developer with great interests in data visualization, creative coding, and human-computer interaction. 
+                <p>A graduate from the University of Guam, where I studied Computer Science and Communication. Currently working as a Research Assistant III at the Water and Environmental Research Institute of the Western Pacific (WERI). An aspiring developer with great interests in data visualization, creative coding, and human-computer interaction. 
                 </p>
         </div>
     )
